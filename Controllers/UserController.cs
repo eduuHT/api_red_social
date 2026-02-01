2e725552449a27b8e3ad890879d6ee10acf107da
Luis Roman <romanborbon111@gmail.com>
2026-02-01 12:14:27 -0700

    Documentacion de Servicio de cliente

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,10 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using api_red_social.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace api_red_social.Controllers
 {
     public class UserController : Controller
     {
-        private readonly 
+        private readonly UserService _userService;
+
+        public UserController(UserService userService)
+        {
+            _userService = userService;
+        }
         public IActionResult Index()
         {
             return View();
--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,21 +1,28 @@
-﻿namespace api_red_social.Services
+﻿using api_red_social.Models;
+using System.Text.Json;
+
+namespace api_red_social.Services
 {
     public class UserService
     {
         private readonly HttpClient _httpClient;
         public UserService(IConfiguration configuration )
-        {
-            var baseAddress = configuration["GitHub:BaseUrl"];
-            _httpClient = new HttpClient
-            {
-                BaseAddress = new Uri(baseAddress)
-            };
+        { 
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri(configuration["GitHubApi:BaseUrl"]); // Agarramos la url base del appsettings.json
         }
-        public async Task<string> GetUserAsync(string username)
+        public async Task<GetUserDTO> GetUserAsync(string username)
         {
-            var response = await _httpClient.GetAsync($"/users/{username}");
-            response.EnsureSuccessStatusCode();
-            return await response.Content.ReadAsStringAsync();
+            var response = await _httpClient.GetAsync($"/users/{username}"); // Le damos de parametro el usuario a buscar
+            response.EnsureSuccessStatusCode(); // Asegura que la peticion fue exitosa, si no lanza una excepcion
+
+            var Json = await response.Content.ReadAsStringAsync(); // Guardamos el contenido de la respuesta como una cadena JSON
+
+            var result = JsonSerializer.Deserialize<GetUserDTO>(Json, new JsonSerializerOptions  // Transformamos el JSON a un objeto GetUserDTO
+            {
+                PropertyNameCaseInsensitive = true // Ignora mayusculas y minusculas en los nombres de las propiedades
+            });
+            return result; // Devolvemos el objeto GetUserDTO
         }
     }
 }
