940f39e0c9883fe9de1d107a2036160e6ade458d
eduuHT <eduuu.ht@gmail.com>
2026-01-31 23:52:48 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace api_red_social.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
